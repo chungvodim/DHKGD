ef92ac140f9b3942b52b0aa29bda192eb870e7e9
chungvodim <ba198647@gmail.com>
2017-10-09 19:49:30 +0700

    refactoring

---
+++ Tearc.Service.Implementation/EmailSender.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tearc.Service.Implementation
+{
+    public class EmailSender : IEmailSender
+    {
+    }
+}
---
+++ Tearc.Service/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tearc.Service
+{
+    public interface IEmailSender
+    {
+    }
+}

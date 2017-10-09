75e5ecca08ffae6578d577a54fe97c8bbfa71bd9
chungvodim <ba198647@gmail.com>
2017-10-09 19:59:09 +0700

    Refactoring

---
+++ Tearc.Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tearc.Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

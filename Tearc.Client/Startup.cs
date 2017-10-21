c778f1e2f991d376429d53c337a27ff646e310e9
chungvodim <ba198647@gmail.com>
2017-10-21 17:45:59 +0700

    update Tearc.Client.csproj

--- Tearc.Client/Startup.cs
+++ Tearc.Client/Startup.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Tearc.Repository.Main;
+using Microsoft.EntityFrameworkCore;
+using Tearc.Entity.Main;
+using Microsoft.AspNetCore.Identity;
+using Tearc.Service;
+using Tearc.Service.Implementation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tearc_Client
 {
     public class Startup
     {
+        string _testSecret = null;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,7 +31,31 @@ namespace Tearc_Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MainDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TearcConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<MainDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            // Requires all requests use HTTPS
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
             services.AddMvc();
+
+            _testSecret = Configuration["MySecret"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
